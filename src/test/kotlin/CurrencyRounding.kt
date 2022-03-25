43ce283bd1967e1f1feda809722e7fd5e509ea1c
NaiPofo <50967056+naipofo@users.noreply.github.com>
2022-03-25 10:56:48 +0100

    Clean up tests

--- src/test/kotlin/CurrencyRounding.kt
+++ src/test/kotlin/CurrencyRounding.kt
@@ -11,19 +11,12 @@ class CurrencyRounding {
 
     @Test
     fun `Adding last zero`() {
-        println(0.0.roundCurrency())
-        println(0.1.roundCurrency())
-        println(0.1.roundCurrency())
         assert(0.1.roundCurrency() == "0.10")
         assert(41.1.roundCurrency() == "41.10")
     }
 
     @Test
     fun `Removing precision`() {
-        println(0.12121.roundCurrency())
-        println(0.12121.roundCurrency())
-        println(0.12121.roundCurrency())
-        println(0.12121.roundCurrency())
         assert(0.12121.roundCurrency() == "0.12")
         assert(0.129.roundCurrency() == "0.13")
     }
